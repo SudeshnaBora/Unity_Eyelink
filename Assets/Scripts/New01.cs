e8a96ab843e8fb99e0ff78a85ab35a2ff0016acd
SudeshnaBora <sudeshnamani1@gmail.com>
2020-01-16 15:00:04 +0100

    inital commit

---
+++ Assets/Scripts/New01.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class New01 : MonoBehaviour
+{
+
+    [DllImport ("eyelink_core.dll")]
+    private static extern int open_eyelink_connection(int x);
+    // Start is called before the first frame update
+
+    [DllImport("eyelink_core.dll")]
+    private static extern uint do_tracker_setup();
+
+    void Start()
+    {
+        int result = open_eyelink_connection(1);
+        Debug.Log(result);
+        // Debug.Log(open_eyelink_connection(0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int result = open_eyelink_connection(1);
+        
+    }
+}
